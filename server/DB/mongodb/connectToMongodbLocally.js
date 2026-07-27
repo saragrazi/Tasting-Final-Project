dc968c2f0ec0e9fb991801e3d3fcc0b839813c7a
saragrazi <saragr328@gmail.com>
2026-07-27 11:35:51 +0300

    connect to mongo

--- server/DB/mongodb/connectToMongodbLocally.js
+++ server/DB/mongodb/connectToMongodbLocally.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 
 mongoose
-    .connect("mongodb://127.0.0.1:27017/Tasting")
-    .then(() => console.log(chalk.magentaBright("connected to MongoDb Locally!")))
+.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Tasting")    .then(() => console.log(chalk.magentaBright("connected to MongoDb Locally!")))
     .catch(error =>
         console.log(chalk.redBright.bold(`could not connect to mongoDb: ${error}`))
     );
