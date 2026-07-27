af701702d38b4e466ff8aa77438d578604ded9be
saragrazi <saragr328@gmail.com>
2026-07-27 10:39:36 +0300

    // Allowed Origins for CORS configuration

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -4,7 +4,14 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5500", "https://my-tasting.onrender.com"],
+    origin: [
+      "http://127.0.0.1:5500",
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://my-tasting.vercel.app",
+      "https://my-tasting.com",
+      "https://www.my-tasting.com",
+    ],
     optionsSuccessStatus: 200,
   })
 );
